b697cf99c7a04be8f7f904edadcd653bb3e3f20d
Marco <marco.palummieri@gmail.com>
2025-04-21 01:25:14 +0200

    Add MovableObject class and update game initialization logic

--- js/game.js
+++ js/game.js
@@ -1,12 +1,10 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init() {
   canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
+  ctx = canvas.getContext("2d"); //Tool zum hinzufügen auf die 2D-Koordinaten.
 
-  character.src = "../img/2_character_pepe/2_walkW-21.png";
-
-  ctx.drawImage(character, 20, 20, 50, 150);
+  console.log("My Character is", character);
 }
