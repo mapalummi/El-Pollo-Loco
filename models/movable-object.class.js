c50a21e9db1b5169640602001ac1ca7091ff477d
Marco <marco.palummieri@gmail.com>
2025-04-22 17:11:38 +0200

    Add Cloud class and enhance MovableObject with image caching and bulk loading

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,11 +4,23 @@ class MovableObject {
   img;
   width = 100;
   height = 200;
+  imageCache = {};
 
   // loadImage('img/test.png');
   loadImage(path) {
     this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
     this.img.src = path;
+  }
+
+  //
+  // @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+  //
+  loadImages(arr) {
+    arr.forEach(path => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = path;
+    });
   }
 
   moveRight() {
