03f934eeecada997cdb77ca37b1c0bdf328e13ef
Marco <marco.palummieri@gmail.com>
2025-04-23 11:25:26 +0200

    Refactor Character class to use imagesWalking array for loading images and fix image caching in MovableObject class

--- models/character.class.js
+++ models/character.class.js
@@ -1,17 +1,30 @@
 class Character extends MovableObject {
   y = 210;
   height = 220;
+  imagesWalking = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
-    this.loadImages([
-      "img/2_character_pepe/2_walk/W-21.png",
-      "img/2_character_pepe/2_walk/W-22.png",
-      "img/2_character_pepe/2_walk/W-23.png",
-      "img/2_character_pepe/2_walk/W-24.png",
-      "img/2_character_pepe/2_walk/W-25.png",
-      "img/2_character_pepe/2_walk/W-26.png",
-    ]);
+    this.loadImages(this.imagesWalking);
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.imagesWalking.length; // Modulo Operator
+      let path = this.imagesWalking[this.currentImage];
+      this.img = this.imageCache[path]; // Warum hier this. ??
+      this.currentImage++;
+    }, 1000);
   }
 
   jump() {}
