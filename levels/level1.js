2a5e2011c9f875c9297b958a54e3ebdf88dbcbca
Marco <marco.palummieri@gmail.com>
2025-04-30 18:49:26 +0200

    Add LittleChicken class and update level1 to include LittleChicken instances

--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,5 @@
 const level1 = new Level(
-  [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
+  [new Chicken(), new Chicken(), new Chicken(), new LittleChicken(), new Endboss()],
 
   [new Cloud(), new Cloud(), new Cloud(), new Cloud()],
 
---
+++ models/little-chicken.class.js
@@ -0,0 +1,49 @@
+class LittleChicken extends MovableObject {
+  x = 0;
+  y = 390;
+  width = 30;
+  height = 40;
+
+  offset = {
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 10,
+  };
+
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+  ];
+
+  IMAGES_DEAD = ["img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
+
+  constructor() {
+    super().loadImage("img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
+    this.loadImages(this.IMAGES_WALKING);
+
+    this.x = 200 + Math.random() * 500; //Zahl zwischen 200 und 700
+    this.speed = 0.15 + Math.random() * 0.25; //Minimalster Wert ist 0.15
+
+    this.animate();
+  }
+
+  getRealFrame() {
+    this.rX = this.x + this.offset.left;
+    this.rY = this.y + this.offset.top;
+    this.rW = this.width - this.offset.left - this.offset.right;
+    this.rH = this.height - this.offset.top - this.offset.bottom;
+  }
+
+  animate() {
+    setInterval(() => {
+      this.getRealFrame(); //Kollisionsbox wird ständig aktualisiert
+      this.moveLeft();
+    }, 1000 / 60);
+
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 200);
+  }
+}
