f21377af7a898b5bd44d9d422fff3513ac766f25
Marco <marco.palummieri@gmail.com>
2025-05-08 20:14:48 +0200

    Add start button functionality and style; implement start screen display

--- js/game.js
+++ js/game.js
@@ -1,21 +1,45 @@
 let canvas;
+
+let ctx; // NEU
+
 let world;
 let gameMusic;
 const keyboard = new Keyboard();
 
 function init() {
-  initLevel(); // Wird dann später mit dem Start-Button verknüpft!?
+  initLevel();
   canvas = document.getElementById("canvas");
+
+  ctx = canvas.getContext("2d"); // NEU
+
   gameMusic = new sound("audio/fast-rocky-loop-1.mp3");
-  gameMusic.enableLoop();
+  gameMusic.enableLoop(); // Schleifenwiedergabe aktivieren
   console.log("Audio geladen", gameMusic.sound.src);
+
+  // Lade und zeige den Startscreen
+  const startScreenImage = new Image();
+  startScreenImage.src = "img/9_intro_outro_screens/start/startscreen_1.png"; // Pfad zum Startscreen-Bild
+  startScreenImage.onload = () => {
+    ctx.drawImage(startScreenImage, 0, 0, canvas.width, canvas.height);
+    drawStartText(); // Text über dem Bild zeichnen
+    document.getElementById("startButton").style.display = "block"; // Button anzeigen
+  };
+}
+
+// NEU - Starttext
+function drawStartText() {
+  ctx.fillStyle = "white";
+  ctx.font = "30px Arial";
+  ctx.textAlign = "center";
+  ctx.fillText("Drücke Start, um das Spiel zu beginnen!", canvas.width / 2, canvas.height - 50);
 }
 
 function startGame() {
   world = new World(canvas, keyboard);
-  gameMusic.play();
-  console.log("Audio geladen", gameMusic.sound.src);
+  gameMusic.play(); // Musik abspielen
 
+  // Startscreen löschen (Geht auch ohne !?)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   document.getElementById("startButton").style.display = "none";
 }
 
@@ -32,21 +56,31 @@ function sound(src) {
   this.sound.style.display = "none";
   document.body.appendChild(this.sound);
 
-  //Methode zum Abspielen
+  // Methode zum Abspielen
   this.play = function () {
     this.sound.play();
   };
 
-  //Methode zum Stoppen
+  // Methode zum Stoppen
   this.stop = function () {
     this.sound.pause();
   };
 
+  // Methode zum aktivieren der Endlossschleife
   this.enableLoop = function () {
-    this.sound.loop = true;
-    this.sound.load();
+    this.sound.loop = true; //Aktiviert Schleifenwiedergabe
+    this.sound.load(); //lädt Audio neu, damit Schleife korrekt startet
   };
 }
+
+// Pause/Resume Audio when leave or return tab
+document.addEventListener("visibilitychange", () => {
+  if (document.hidden) {
+    gameMusic.stop();
+  } else {
+    gameMusic.play().catch(e => console.log("Auto-resume prevented:", e));
+  }
+});
 
 window.addEventListener("keydown", e => {
   switch (e.key) {
